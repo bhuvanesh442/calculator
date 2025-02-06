662022f42a8684eb494efb97c547b432a467427a
bhuvanesh442 <bhuvanesh02052005@gmail.com>
2025-02-06 22:33:05 +0530

    display

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Calculator() {
+  const [displayvalue, setdisplayvalue] = useState("");
+
+  const handlebuttonclick = (value) => {
+    setdisplayvalue((prev) => prev + value);
+}
+
+
   return (
     <>
     <h1>Calculator</h1>
     <div className="calculator">
-      <div className="display"></div>
+      <div className="display">{displayvalue}</div>
       <div className="buttons">
         {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "C", "+"].map((btn) => (
-          <button className={["/", "*", "-", "+", "="].includes(btn) ? "symbol" : ""}>
+          <button onClick={() => handlebuttonclick(btn)} className={["/", "*", "-", "+", "="].includes(btn) ? "symbol" : ""}>
             {btn}
           </button>
         ))}
-        <button className="symbol">=</button>
+        <button className="equals">=</button>
       </div>
     </div>
   </>
