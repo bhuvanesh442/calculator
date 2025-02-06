01916b95ae053978afa1d5fd9af3ae1678a79d9a
bhuvanesh442 <bhuvanesh02052005@gmail.com>
2025-02-06 22:02:10 +0530

    gp

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -3,47 +3,18 @@ import React from "react";
 export default function Calculator() {
   return (
     <>
-      <h1>Calculator</h1>
-
-      <div className="calculator">
-        <div className="display"></div>
-
-        <div className="buttons">
-          <button>7</button>
-
-          <button>8</button>
-
-          <button>9</button>
-
-          <button className="symbol">/</button>
-
-          <button>4</button>
-
-          <button>5</button>
-
-          <button>6</button>
-
-          <button className="symbol">*</button>
-
-          <button>1</button>
-
-          <button>2</button>
-
-          <button>3</button>
-
-          <button className="symbol">-</button>
-
-          <button>0</button>
-
-          <button>.</button>
-
-          <button>C</button>
-
-          <button>+</button>
-
-          <button>=</button>
-        </div>
+    <h1>Calculator</h1>
+    <div className="calculator">
+      <div className="display"></div>
+      <div className="buttons">
+        {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "C", "+"].map((btn) => (
+          <button  >
+            {btn}
+          </button>
+        ))}
+        <button>=</button>
       </div>
-    </>
-  );
-}
+    </div>
+  </>
+);
+};
